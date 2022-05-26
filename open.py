2f7300a118f430c4afb1ccdc27c97c270867b601
ARAMUSHA131 <market120120171@gmail.com>
2022-05-26 21:36:49 +0300

    Issues fixed

--- open.py
+++ open.py
@@ -3,17 +3,21 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route('/open/')
 def list_files():
     filenames = os.listdir('open')
     return render_template('open.html', files=filenames)
 
+
 @app.route('/open/<path:filename>')
-def send(filename):
+def expose_file_from_directory(filename):
     return send_from_directory(
         os.path.abspath('open'),
         filename,
         as_attachment=True
     )
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6001)
+    app.run(host='0.0.0.0', port=6000)
